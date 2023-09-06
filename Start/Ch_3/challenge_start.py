a77b05f330a8094e669d613ea2c356b81a755dd9
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-09-06 15:28:45 +0000

    comprehensions done

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -1,15 +1,24 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # Programming challenge for comprehensions
 
-import string
+from string import punctuation as pct
 import pprint
 
 
-test_str = "2 apples, 9 oranges?, 4 pears, Mike's 1 egg, Jane's 2 kiwis, $50!"
+TEST_STR = "2 apples, 9 oranges?, 4 pears, Mike's 1 egg, Jane's 2 kiwis, $50!"
 
 # YOUR CODE HERE
+num_digits = len([digit for digit in TEST_STR if digit.isdigit()])
+num_pct = len([punct for punct in TEST_STR if punct in pct])
+NUM_CHARS = len(TEST_STR)
+UNQ_LTTR_STR = ''.join({char for char in TEST_STR if char.isalpha()})
 
 # print the data
 str_data = {
+    'Length': NUM_CHARS,
+    'Digits': num_digits,
+    'Punctuation': num_pct,
+    'Unique Letters': UNQ_LTTR_STR,
+    'Unique Count': len(UNQ_LTTR_STR)
 }
 pprint.pp(str_data)
--- Start/Ch_3/listcomp_start.py
+++ Start/Ch_3/listcomp_start.py
@@ -7,7 +7,12 @@ evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 # TODO: Perform a mapping and filter function on a list
+print(list(filter(lambda x: x < 10, map(lambda x: x + 1, evens))))
 
 # TODO: Derive a new list of numbers frm a given list
+squared_odds = [odd ** 2 for odd in odds]
+print(squared_odds)
 
 # TODO: Limit the items operated on with a predicate condition
+double_small_odds = [(odd * 2) for odd in odds if odd < 10]
+print(double_small_odds)
